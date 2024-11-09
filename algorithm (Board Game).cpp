ae6335bdfe0c5a4ede3ade3b66271ef29ac5a60b
LeeYS0221 <jangsm5037@gmail.com>
2024-11-09 16:52:16 +0900

    ADD : Test 2

---
+++ algorithm (Board Game).cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<iostream>
+#include<cstring> 
+using namespace std;
+
+int board[51]={0, };
+int move(int now){
+	
+}
+int main(){
+	int N;
+	int M[49];
+	char C[49];
+	int D;
+	cin >> N;
+	for(int i=0; i<N; i++){
+		cin >> M[i] >> C[i];
+	}
+	cin >> D;
+	
+}
---
+++ algorithm (Nickname).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+#include<cstring> 
+using namespace std;
+int main(){
+	string M;
+	string O;
+	cin >> M >> O;
+	if(M>O){
+		cout << "sir";
+	}
+	else{
+		cout << "kid";
+	}
+
+	return 0;
+}
